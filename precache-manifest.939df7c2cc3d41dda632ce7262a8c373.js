73491a0e4a3a16be9edcaf6a59dc4838d833f37a
Vladyslav Braiko <vladyslavbar@gmail.com>
2020-07-08 13:02:20 +0300

    Updates

---
+++ precache-manifest.939df7c2cc3d41dda632ce7262a8c373.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41216809e43b51f5fce2be80aec7920e",
+    "url": "/TWC/index.html"
+  },
+  {
+    "revision": "818383f12d5f73e76ca5",
+    "url": "/TWC/static/css/main.db007aa1.chunk.css"
+  },
+  {
+    "revision": "6f1d14d3cc849425d006",
+    "url": "/TWC/static/js/2.a847a084.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TWC/static/js/2.a847a084.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "818383f12d5f73e76ca5",
+    "url": "/TWC/static/js/main.0f71bff7.chunk.js"
+  },
+  {
+    "revision": "6d4c1f12d285e3cea0b1",
+    "url": "/TWC/static/js/runtime-main.eb443c80.js"
+  }
+]);
